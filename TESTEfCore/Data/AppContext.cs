99319544a745fa2f6ccb3346cd683532a1245267
HappyFox002 <45999287+HappyFox002@users.noreply.github.com>
2022-01-19 16:11:12 +0300

    Init

---
+++ TESTEfCore/Data/AppContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TESTEfCore.Data
+{
+    public class AppContext : DbContext
+    {
+        public AppContext() {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;user=root;password=root;database=todolist;");
+        }
+    }
+}
---
+++ TESTEfCore/Models/Test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TESTEfCore.Models
+{
+    public class Test
+    {
+        public int id { get; set; }
+        public string test { get; set; }
+    }
+}
