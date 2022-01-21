8441567ba9de7d505c1ac74ca311c4b7b66c2082
HappyFox002 <45999287+HappyFox002@users.noreply.github.com>
2022-01-21 17:18:04 +0300

    Work ui jquery

---
+++ TESTEfCore/TESTEfCore/wwwroot/js/app.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+    $('.task').on('click', function () {
+        if ($(this).hasClass('tActive')) {
+            $(this).toggleClass('tActive');
+            $('.taskDetailed').toggleClass('tDOff');
+        }
+        else
+        {
+            $('.task').each(function (elem) {
+                if ($(this).hasClass('tActive')) {
+                    $(this).toggleClass('tActive');
+                }
+            });
+            $(this).toggleClass('tActive');
+            if (!$('.taskDetailed').hasClass('.taskDetailed')) {
+                $('.taskDetailed').toggleClass('tDOff');
+            }
+        }
+    });
+
+    $('#addTask').on('click', function () {
+        $('.modalNewTask').toggleClass('mOff');
+    });
+
+    $('#addTaskClose').on('click', function () {
+        $('.modalNewTask').toggleClass('mOff');
+    });
+});
